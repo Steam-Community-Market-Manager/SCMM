851f7203cdc2338445edb283d25bd792d18a65b9
Rhys Koedijk <rhys@koedijk.co.nz>
2020-06-19 22:05:47 +1200

    Added donation option and cleaned up application layout/navigation views

--- SCMM.Web/Client/Shared/Component/Layout/NavigationItem.cs
+++ SCMM.Web/Client/Shared/Component/Layout/NavigationItem.cs
@@ -9,6 +9,8 @@ namespace SCMM.Web.Client.Shared.Component.Layout
 
         public string Title { set; get; }
 
+        public string SubTitle { get; set; }
+
         public string Path { set; get; }
 
         public Action OnClick { set; get; }
