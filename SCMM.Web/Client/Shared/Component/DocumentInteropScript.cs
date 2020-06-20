cfaef5c812bad1999d8c2ff95bd8d9957f47d378
Rhys Koedijk <rhys@koedijk.co.nz>
2020-06-21 10:34:48 +1200

    Added document title component, set unique page titles

---
+++ SCMM.Web/Client/Shared/Component/DocumentInteropScript.cs
@@ -0,0 +1,17 @@
+﻿using Skclusive.Core.Component;
+
+namespace SCMM.Web.Client.Shared.Chart
+{
+    public class DocumentInteropScript : ScriptComponentBase
+    {
+        protected override string GetScript()
+        {
+            return @"
+                var DocumentInterop = DocumentInterop || {};
+                DocumentInterop.setDocumentTitle = function (title) {
+                    document.title = title;
+                };
+            ";
+        }
+    }
+}
