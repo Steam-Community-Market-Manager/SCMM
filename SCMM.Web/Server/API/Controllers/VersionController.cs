43f300441e94ef66346463256f168806195af0ed
Rhys Koedijk <rhys@koedijk.co.nz>
2020-07-08 00:03:37 +1200

    Added client/server version info to app sidebar footer

---
+++ SCMM.Web/Server/API/Controllers/VersionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace SCMM.Web.Server.API.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : ControllerBase
+    {
+        private readonly ILogger<StoreItemsController> _logger;
+
+        public VersionController(ILogger<StoreItemsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var server = Assembly.GetExecutingAssembly().GetName();
+            return $"{server.Version.Major}.{server.Version.Minor}";
+        }
+    }
+}
