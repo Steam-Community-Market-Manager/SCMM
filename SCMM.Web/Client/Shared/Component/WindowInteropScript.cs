4914aaf28a91fc57ebcce44d107a75ecade84d89
Rhys Koedijk <rhys@koedijk.co.nz>
2020-07-30 20:28:19 +1200

    Added WindowInteropScript object to handle external url navigation

---
+++ SCMM.Web/Client/Shared/Component/WindowInteropScript.cs
@@ -0,0 +1,17 @@
+﻿using Skclusive.Core.Component;
+
+namespace SCMM.Web.Client.Shared.Component
+{
+    public class WindowInteropScript : ScriptComponentBase
+    {
+        protected override string GetScript()
+        {
+            return @"
+                var WindowInterop = WindowInterop || {};
+                WindowInterop.openInNewTab = function (url) {
+                    window.open(url, '_blank');
+                };
+            ";
+        }
+    }
+}
