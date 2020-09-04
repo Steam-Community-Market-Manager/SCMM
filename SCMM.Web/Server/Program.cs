167ce14e26294bbcb08611013c62e3044b958080
Rhys Koedijk <rhys@koedijk.co.nz>
2020-09-05 09:55:04 +1200

    Azure deployment profile clean-ups

--- SCMM.Web/Server/Program.cs
+++ SCMM.Web/Server/Program.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.ApplicationInsights;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace SCMM.Web.Server
 {
@@ -14,6 +17,15 @@ namespace SCMM.Web.Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddAzureKeyVault(
+                        new Uri(
+                            Environment.GetEnvironmentVariable("ASPNETCORE_HOSTINGSTARTUP__KEYVAULT__CONFIGURATIONVAULT")
+                        ),
+                        new DefaultAzureCredential()
+                    );
+                })
                 .ConfigureLogging(logging =>
                 {
                     logging.ClearProviders();
