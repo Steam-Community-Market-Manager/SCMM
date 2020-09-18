aeba1583e03841655718711feb7a27d2df0d76a3
Rhys Koedijk <rhys@koedijk.co.nz>
2020-09-18 21:43:05 +1200

    Added role-based flairs that appear below the profile avatar and name in the navigation sidebar

--- SCMM.Web/Shared/Domain/Roles.cs
+++ SCMM.Web/Shared/Domain/Roles.cs
@@ -5,6 +5,7 @@
         public const string Administrator = "Administrator";
         public const string Creator = "Creator";
         public const string Donator = "Donator";
+        public const string VIP = "VIP";
         public const string User = "User";
     }
 }
