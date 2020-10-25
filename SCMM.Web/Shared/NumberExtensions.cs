b56c33f09479f5490ebd9e03ac14105160379b93
Rhys Koedijk <rhys@koedijk.co.nz>
2020-10-26 00:50:10 +1300

    Added a "marketplace vs store" price comparison chart to the store page

--- SCMM.Web/Shared/NumberExtensions.cs
+++ SCMM.Web/Shared/NumberExtensions.cs
@@ -63,6 +63,25 @@ namespace SCMM.Web.Shared
             return (long)Math.Floor(localValue);
         }
 
+        public static decimal ToPrice<T>(this T currency, long price, IExchangeableCurrency priceCurrency)
+            where T : ICurrency, IExchangeableCurrency
+        {
+            return currency?.ToPrice(currency.CalculateExchange(price, priceCurrency)) ?? 0;
+        }
+
+        public static decimal ToPrice(this ICurrency currency, long price)
+        {
+            if (currency == null)
+            {
+                return (decimal) price;
+            }
+
+            var localScaleString = String.Empty.PadRight(currency.Scale, '0');
+            var localScaleDivisor = Int64.Parse($"1{localScaleString}");
+            var localPrice = Math.Round((decimal)Math.Abs(price) / localScaleDivisor, currency.Scale);
+            return localPrice;
+        }
+
         public static string ToPriceString<T>(this T currency, long price, IExchangeableCurrency priceCurrency, bool dense = false)
             where T : ICurrency, IExchangeableCurrency
         {
@@ -78,10 +97,9 @@ namespace SCMM.Web.Shared
 
             var sign = (price < 0) ? "-" : String.Empty;
             var localScaleString = String.Empty.PadRight(currency.Scale, '0');
-            var localScaleDivisor = Int64.Parse($"1{localScaleString}");
             var localFormat = $"#,##0{(currency.Scale > 0 ? "." : String.Empty)}{localScaleString}";
             var localCulture = CultureInfo.GetCultureInfo(currency.CultureName);
-            var localPrice = Math.Round((decimal)Math.Abs(price) / localScaleDivisor, currency.Scale);
+            var localPrice = currency.ToPrice(price);
             var localPriceString = $"{sign}{localPrice.ToString(localFormat, localCulture.NumberFormat)}";
             if (!dense)
             {
