5ddaac3220fcd2f187a8b6a102258e450d5a0260
Rhys Koedijk <rhys@koedijk.co.nz>
2020-11-06 00:27:55 +1300

    Added context menu component

---
+++ SCMM.Web/Client/Shared/Component/ContextMenu/ContextContainer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Skclusive.Material.Core;
+using Skclusive.Material.Menu;
+
+namespace SCMM.Web.Client.Shared.Component.ContextMenu
+{
+    public class ContextContainerComponent : MaterialComponent
+    {
+        public ContextContainerComponent() : base("ContextContainer")
+        {
+        }
+
+        [Parameter]
+        public RenderFragment ContextMenu { get; set; }
+
+        [Parameter]
+        public bool Open { set; get; }
+
+        protected double MouseX { get; set; }
+
+        protected double MouseY { get; set; }
+
+        protected void HandleMenuOpen(MouseEventArgs args)
+        {
+            // TODO: Make this accessible on mobile devices by implementing a "long click" option
+            if (args.Button == 2)
+            {
+                MouseX = args.ClientX;
+                MouseY = args.ClientY;
+                Open = true;
+                StateHasChanged();
+            }
+        }
+
+        protected void HandleMenuClose(EventArgs args)
+        {
+            CloseMenu();
+        }
+
+        protected void HandleMenuClose(MenuCloseReason reason)
+        {
+            CloseMenu();
+        }
+
+        public void CloseMenu()
+        {
+            Open = false;
+            StateHasChanged();
+        }
+    }
+}
