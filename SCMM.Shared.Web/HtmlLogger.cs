2db6b8b842df2bdbd18138b6d4ffb36fb785002d
Rhys Koedijk <rhys@koedijk.co.nz>
2021-05-01 18:46:19 +1200

    Added HtmlLogger provider

---
+++ SCMM.Shared.Web/HtmlLogger.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace SCMM.Shared.Web
+{
+    public class HtmlLogger : ILoggerProvider, ILogger
+    {
+        private static readonly List<string> Logs = new List<string>();
+        private const int MaxLogCount = 300;
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            return;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var background = "inherit";
+            var foreground = "inherit";
+            var gray = "gray";
+            switch (logLevel)
+            {
+                case LogLevel.Warning: background = "darkgoldenrod"; gray = "lightgray"; break;
+                case LogLevel.Error: background = "darkred"; gray = "lightgray"; break;
+                case LogLevel.Critical: background = "darkred"; break;
+            }
+
+            var log = $"<span style=\"color:{gray}\">[{DateTimeOffset.Now.ToString("yyyy-MM-dd HH:mm:ss")}]</span> <strong><samp>{logLevel.ToString().ToUpper()}</samp></strong> <span>{formatter(state, exception)}</span>";
+            if (!String.IsNullOrEmpty(exception?.StackTrace))
+            {
+                log += $"<blockquote>{exception.StackTrace}</blockquote>";
+            }
+
+            Logs.Add($"<span style=\"background-color:{background};color:{foreground}\">{log}</span><br/>");
+            var overcommited = (Logs.Count - MaxLogCount);
+            if (overcommited > 0)
+            {
+                Logs.RemoveRange(0, overcommited);
+            }
+        }
+
+        public static string Buffer
+        {
+            get
+            {
+                var inverseLogs = new List<string>(Logs);
+                inverseLogs.Reverse();
+                return String.Join('\n', inverseLogs);
+            }
+        }
+    }
+
+    public static class HtmlLoggerExtensions
+    {
+        public static ILoggingBuilder AddHtmlLogger(this ILoggingBuilder builder)
+        {
+            return builder.AddProvider(new HtmlLogger());
+        }
+    }
+}
