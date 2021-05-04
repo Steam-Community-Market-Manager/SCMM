7619766d7d0f56f5686658fa4dfc2eea29391554
Rhys Koedijk <rhys@koedijk.co.nz>
2021-05-04 19:18:14 +1200

    Log an info message when currency exchange rates are updated in Web.Server

--- SCMM.Web.Server/Handlers/CurrencyExchangeRateUpdateMessageHandler.cs
+++ SCMM.Web.Server/Handlers/CurrencyExchangeRateUpdateMessageHandler.cs
@@ -1,4 +1,5 @@
-﻿using SCMM.Shared.Azure.ServiceBus;
+﻿using Microsoft.Extensions.Logging;
+using SCMM.Shared.Azure.ServiceBus;
 using SCMM.Shared.Azure.ServiceBus.Attributes;
 using SCMM.Steam.API.Messages;
 using System.Threading.Tasks;
@@ -8,16 +9,19 @@ namespace SCMM.Web.Server.Handlers
     [Concurrency(MaxConcurrentCalls = 100)]
     public class CurrencyExchangeRateUpdateMessageHandler : IMessageHandler<CurrencyExchangeRateUpdateMessage>
     {
+        private readonly ILogger<CurrencyExchangeRateUpdateMessageHandler> _logger;
         private readonly CurrencyCache _currencyCache;
 
-        public CurrencyExchangeRateUpdateMessageHandler(CurrencyCache currencyCache)
+        public CurrencyExchangeRateUpdateMessageHandler(ILogger<CurrencyExchangeRateUpdateMessageHandler> logger, CurrencyCache currencyCache)
         {
+            _logger = logger;
             _currencyCache = currencyCache;
         }
 
         public Task HandleAsync(CurrencyExchangeRateUpdateMessage message)
         {
             _currencyCache.UpdateExchangeRate(message.Currency, message.ExchangeRateMultiplier);
+            _logger.LogInformation($"Currency {message.Currency} exchange rate multiple has been updated to {message.ExchangeRateMultiplier}");
             return Task.CompletedTask;
         }
     }
