e0723cb525606e8a263e3d1747feecd8ad814b48
Rhys Koedijk <rhys@koedijk.co.nz>
2021-06-24 20:39:53 +1200

    Added admin Discord commands for managing user roles

--- SCMM.Discord.Bot.Server/Modules/AdministrationModule.Profile.cs
+++ SCMM.Discord.Bot.Server/Modules/AdministrationModule.Profile.cs
@@ -1,6 +1,7 @@
 ﻿using Discord.Commands;
 using Microsoft.EntityFrameworkCore;
 using SCMM.Discord.Client;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,70 @@ namespace SCMM.Discord.Bot.Server.Modules
 {
     public partial class AdministrationModule
     {
+        [Command("roles")]
+        [Alias("role")]
+        public async Task<RuntimeResult> ListRoleAsync(string steamId)
+        {
+            var profile = await _db.SteamProfiles
+                .Where(x => x.SteamId == steamId || x.ProfileId == steamId)
+                .FirstOrDefaultAsync();
+
+            if (profile != null)
+            {
+                return CommandResult.Success(String.Join(", ", profile.Roles));
+            }
+            else
+            {
+                return CommandResult.Fail("Unable to find a profile");
+            }
+        }
+
+        [Command("role add")]
+        public async Task<RuntimeResult> AddRoleAsync(string steamId, [Remainder] string role)
+        {
+            var profile = await _db.SteamProfiles
+                .Where(x => x.SteamId == steamId || x.ProfileId == steamId)
+                .FirstOrDefaultAsync();
+
+            if (profile != null)
+            {
+                if (!profile.Roles.Contains(role))
+                {
+                    profile.Roles.Add(role);
+                    await _db.SaveChangesAsync();
+                }
+
+                return CommandResult.Success(String.Join(", ", profile.Roles));
+            }
+            else
+            {
+                return CommandResult.Fail("Unable to find a profile");
+            }
+        }
+
+        [Command("role remove")]
+        public async Task<RuntimeResult> RemoveRoleAsync(string steamId, [Remainder] string role)
+        {
+            var profile = await _db.SteamProfiles
+                .Where(x => x.SteamId == steamId || x.ProfileId == steamId)
+                .FirstOrDefaultAsync();
+
+            if (profile != null)
+            {
+                if (profile.Roles.Contains(role))
+                {
+                    profile.Roles.Remove(role);
+                    await _db.SaveChangesAsync();
+                }
+
+                return CommandResult.Success(String.Join(", ", profile.Roles));
+            }
+            else
+            {
+                return CommandResult.Fail("Unable to find a profile");
+            }
+        }
+
         [Command("donator")]
         public async Task<RuntimeResult> UpdateDonatorAsync(string steamId, [Remainder] int? donatorLevel = null)
         {
@@ -24,7 +89,7 @@ namespace SCMM.Discord.Bot.Server.Modules
                 }
 
                 return CommandResult.Success(
-                    $"Donator level is {profile.DonatorLevel}"
+                    $"Donator level is **{profile.DonatorLevel}**"
                 );
             }
             else
