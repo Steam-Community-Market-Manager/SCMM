36ef72a9a26ee7fa017df9dd64fd1443b2c2ec13
Rhys Koedijk <rhys@koedijk.co.nz>
2021-08-18 16:31:35 +1200

    Added support for image analysis using Azure AI Computer Vision

--- SCMM.Azure.AI/AzureAiClient.cs
+++ SCMM.Azure.AI/AzureAiClient.cs
@@ -1,19 +1,50 @@
 ﻿using Azure;
 using Azure.AI.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 
 namespace SCMM.Azure.AI
 {
     public class AzureAiClient
     {
+        private readonly ComputerVisionClient _computerVisionClient;
         private readonly TextAnalyticsClient _textAnalyticsClient;
 
         public AzureAiClient(AzureAiConfiguration config)
         {
-            _textAnalyticsClient = new TextAnalyticsClient(new Uri(config.Endpoint), new AzureKeyCredential(config.ApiKey));
+            if (config.ComputerVision != null)
+            {
+                _computerVisionClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(config.ComputerVision.ApiKey))
+                {
+                    Endpoint = config.ComputerVision.Endpoint
+                };
+            }
+            if (config.TextAnalytics != null)
+            {
+                _textAnalyticsClient = new TextAnalyticsClient(new Uri(config.TextAnalytics.Endpoint), new AzureKeyCredential(config.TextAnalytics.ApiKey));
+            }
+        }
+
+        public async Task<ImageAnalysis> AnalyseImageAsync(Stream image, params VisualFeatureTypes?[] visualFeatures)
+        {
+            if (_computerVisionClient == null)
+            {
+                throw new InvalidOperationException("Computer vision is not configured");
+            }
+
+            return await _computerVisionClient.AnalyzeImageInStreamAsync(
+                image,
+                visualFeatures: visualFeatures?.ToList()
+            );
         }
 
         public async Task<TextSentiment> GetTextSentimentAsync(string text)
         {
+            if (_textAnalyticsClient == null)
+            {
+                throw new InvalidOperationException("Text analytics is not configured");
+            }
+
             var response = await _textAnalyticsClient.AnalyzeSentimentAsync(text);
             return response?.Value?.Sentiment ?? TextSentiment.Neutral;
         }
--- SCMM.Azure.AI/AzureAiConfiguration.cs
+++ SCMM.Azure.AI/AzureAiConfiguration.cs
@@ -2,6 +2,13 @@
 {
     public class AzureAiConfiguration
     {
+        public AzureAiServiceConfiguration ComputerVision { get; set; }
+
+        public AzureAiServiceConfiguration TextAnalytics { get; set; }
+    }
+
+    public class AzureAiServiceConfiguration
+    {
         public string Endpoint { get; set; }
 
         public string ApiKey { get; set; }
