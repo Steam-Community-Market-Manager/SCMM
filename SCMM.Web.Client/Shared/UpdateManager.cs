ada555efa68aee0f359906399071042884714eeb
Rhys Koedijk <rhys@koedijk.co.nz>
2021-08-20 11:33:36 +1200

    Improved PWA update notifications

---
+++ SCMM.Web.Client/Shared/UpdateManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.JSInterop;
+using SCMM.Web.Client;
+
+public class UpdateManager
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public UpdateManager(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task SetInstanceAsync(DotNetObjectReference<App> instance)
+    {
+        await _jsRuntime.InvokeVoidAsync("UpdateInterop.setInstance", instance);
+    }
+
+    public async Task<bool> IsUpdatePendingAsync()
+    {
+        return await _jsRuntime.InvokeAsync<bool>("UpdateInterop.isUpdatePending");
+    }
+
+    public async Task ApplyPendingUpdateAsync()
+    {
+        await _jsRuntime.InvokeVoidAsync("UpdateInterop.applyPendingUpdate");
+    }
+}
