fd613d13e1595ea51eff824863c8d1e62f7fbb3c
Rhys Koedijk <rhys@koedijk.co.nz>
2021-09-14 23:36:37 +1200

    Added ResponsiveComponent for easier detection of page breakpoints

---
+++ SCMM.Web.Client/Shared/Components/ResponsiveComponent.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using MudBlazor.Services;
+
+namespace SCMM.Web.Client.Shared.Dialogs;
+
+public abstract class ResponsiveComponent : ComponentBase
+{
+    [Inject]
+    protected IResizeListenerService ResizeListener { get; set; }
+
+    protected Breakpoint Breakpoint { get; set; } 
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            ResizeListener.OnBreakpointChanged += OnResized;
+            OnResized(this, await ResizeListener.GetBreakpoint());
+        }
+
+        await base.OnAfterRenderAsync(firstRender);
+    }
+
+    protected virtual void OnBreakpointChanged(Breakpoint breakpoint)
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    private void OnResized(object sender, Breakpoint breakpoint)
+    {
+        Breakpoint = breakpoint;
+        OnBreakpointChanged(breakpoint);
+    }
+
+    protected bool IsMediaSize(Breakpoint breakpoint)
+    {
+        return ResizeListener.IsMediaSize(breakpoint, Breakpoint);
+    }
+
+    public void Dispose()
+    {
+        ResizeListener.OnBreakpointChanged -= OnResized;
+    }
+}
--- SCMM.Web.Client/Shared/Dialogs/ResponsiveDialog.cs
+++ SCMM.Web.Client/Shared/Dialogs/ResponsiveDialog.cs
@@ -1,50 +1,32 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MudBlazor;
-using MudBlazor.Services;
 
 namespace SCMM.Web.Client.Shared.Dialogs;
 
-public abstract class ResponsiveDialog : ComponentBase
+public abstract class ResponsiveDialog : ResponsiveComponent
 {
     [CascadingParameter]
     public MudDialogInstance Dialog { get; set; }
 
     protected ResponsiveDialogOptions Options { get; set; }
 
-    [Inject]
-    protected IResizeListenerService ResizeListener { get; set; }
-
-    protected abstract void OnConfigure(ResponsiveDialogOptions options);
-
     protected override async Task OnInitializedAsync()
     {
+        await base.OnInitializedAsync();
+
         Options = new ResponsiveDialogOptions(Dialog.Options);
         OnConfigure(Options);
         Dialog.SetOptions(Options);
     }
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            ResizeListener.OnBreakpointChanged += OnResized;
-            OnResized(ResizeListener, await ResizeListener.GetBreakpoint());
-        }
+    protected abstract void OnConfigure(ResponsiveDialogOptions options);
 
-        await base.OnAfterRenderAsync(firstRender);
-    }
-
-    private void OnResized(object sender, Breakpoint breakpoint)
+    protected override void OnBreakpointChanged(Breakpoint breakpoint)
     {
         Options.FullScreen = (breakpoint <= Options.FullscreenBreakpoint);
         Options.NoHeader = (Options.FullScreen == true ? false : Dialog.Options.NoHeader);
         Options.CloseButton = (Options.FullScreen == true ? true : Dialog.Options.CloseButton);
         Dialog.SetOptions(Options);
         InvokeAsync(StateHasChanged);
-    }
-
-    public void Dispose()
-    {
-        ResizeListener.OnBreakpointChanged -= OnResized;
     }
 }
