09e4ffe9b4bddebc030cb4cbc77aa407c8293999
Rhys Koedijk <rhys@koedijk.co.nz>
2021-09-20 15:13:59 +1200

    Basic support for components with persistent state

---
+++ SCMM.Web.Client/Shared/Components/PersistentComponent.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SCMM.Web.Client.Shared.Components;
+
+public abstract class PersistentComponent : ComponentBase, IDisposable
+{
+    [Inject]
+    private PersistentComponentState ApplicationState { get; set; }
+
+    private PersistingComponentStateSubscription? StateSubscription { get; set; }
+
+    protected override void OnInitialized()
+    {
+        StateSubscription = ApplicationState?.RegisterOnPersisting(() =>
+        {
+            OnPersistState();
+            return Task.CompletedTask;
+        });
+
+        OnLoadState();
+    }
+
+    protected virtual void OnLoadState()
+    {
+    }
+
+    protected virtual void OnPersistState()
+    {
+    }
+
+    protected T LoadFromState<T>(string name)
+    {
+        T value = default(T);
+        ApplicationState.TryTakeFromJson(name, out value);
+        return value;
+    }
+
+    protected void PersistToState<T>(string name, T value)
+    {
+        ApplicationState.PersistAsJson(name, value);
+    }
+
+    public void Dispose()
+    {
+        StateSubscription?.Dispose();
+    }
+}
