3d7fbb95de92a25290742d968ac02df3b4f45b8b
Rhys Koedijk <rhys@koedijk.co.nz>
2022-01-04 00:50:58 +1300

    Documentation

--- SCMM.Steam.Data.Models/Enums/PriceType.cs
+++ SCMM.Steam.Data.Models/Enums/PriceType.cs
@@ -14,27 +14,35 @@ namespace SCMM.Steam.Data.Models.Enums
 
         [Display(Name = "Steam Community Market")]
         [BuyFrom(Url = "https://steamcommunity.com/market/listings/{0}/{3}")]
+        // TODO: SellFrom
         [SalesTax(13)]
         SteamCommunityMarket,
 
         [Display(Name = "Skinport")]
         [BuyFrom(Url = "https://skinport.com/{1}/market?r=scmm&item={3}")]
+        // TODO: SellFrom
         [SalesTax(13)]
         Skinport,
 
         [Display(Name = "LOOT.Farm")]
         [BuyFrom(Url = "https://loot.farm/")]
+        // TODO: SellFrom
+        // TODO: TradeWith
         [SalesTax(5)]
         LOOTFarm,
 
         [Display(Name = "swap.gg")]
         [BuyFrom(Url = "https://market.swap.gg/browse?idev_id=326&appId={0}&search={3}")]
+        // TODO: SellFrom
+        // TODO: TradeWith
         [SalesTax(5)]
         SwapGG,
 
-        // TODO: Sale tax seems like a fixed fee
         [Display(Name = "Tradeit.gg")]
         [BuyFrom(Url = "https://tradeit.gg/{1}/store?aff=scmm&search={3}")]
+        // TODO: SellFrom
+        // TODO: TradeWith
+        // TODO: Sale tax seems like a fixed fee
         TradeitGG,
 
         // TODO: Implement this...
@@ -42,11 +50,15 @@ namespace SCMM.Steam.Data.Models.Enums
         // https://api.dmarket.com/price-aggregator/v1/aggregated-prices?Titles=Blackout%20Hoodie&Titles=Blackout%20Pants&Limit=100
         [Display(Name = "Dmarket")]
         [BuyFrom(Url = "https://dmarket.com/ingame-items/item-list/{1}-skins?title={3}")]
+        // TODO: SellFrom
+        // TODO: TradeWith
         [SalesTax(7)]
         Dmarket,
 
         [Display(Name = "CS.Deals")]
         [BuyFrom(Url = "https://cs.deals/market/{1}/?name={3}&sort=price")]
+        // TODO: SellFrom
+        // TODO: TradeWith
         [SalesTax(2)]
         CSDeals
     }
