2daaf15719a5b1a4ffe05c30d2cff879afdb3722
Rhys Koedijk <rhys@koedijk.co.nz>
2022-01-23 19:47:27 +1300

    Support for Rust.TM web client

---
+++ SCMM.Market.RustTM.Client/RustTMWebClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace SCMM.Market.RustTM.Client
+{
+    public class RustTMWebClient
+    {
+        private const string BaseUri = "https://rust.tm/api/v2/";
+
+        public async Task<IEnumerable<RustTMItem>> GetPricesAsync(string currencyName = "USD")
+        {
+            using (var client = new HttpClient())
+            {
+                var url = $"{BaseUri}prices/{currencyName.ToUpper()}.json";
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var textJson = await response.Content.ReadAsStringAsync();
+                var responseJson = JsonSerializer.Deserialize<RustTMInventoryDataResponse>(textJson);
+                return responseJson?.Items;
+            }
+        }
+    }
+}
---
+++ SCMM.Market.RustTM.Client/RustTMtem.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SCMM.Market.RustTM.Client
+{
+    public class RustTMItem
+    {
+        [JsonPropertyName("market_hash_name")]
+        public string MarketHashName { get; set; }
+
+        [JsonPropertyName("volume")]
+        public int Volume { get; set; }
+
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+    }
+}
