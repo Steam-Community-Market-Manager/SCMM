533bf5eb97fca4a965fabc080faf7f91ad0c12ec
Rhys Koedijk <rhys@koedijk.co.nz>
2022-01-23 19:47:09 +1300

    Support for CS.Trade web client

---
+++ SCMM.Market.CSTrade.Client/CSTradeItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SCMM.Market.CSTrade.Client
+{
+    public class CSTradeItem
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("app_id")]
+        public long AppId { get; set; }
+
+        [JsonPropertyName("market_hash_name")]
+        public string MarketHashName { get; set; }
+
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("bot")]
+        public string Bot { get; set; }
+
+        [JsonPropertyName("bot_id")]
+        public string BotId { get; set; }
+    }
+}
---
+++ SCMM.Market.CSTrade.Client/CSTradeWebClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace SCMM.Market.CSTrade.Client
+{
+    public class CSTradeWebClient
+    {
+        private const string BaseUri = "https://cdn.cs.trade:8443/api/";
+
+        public async Task<IEnumerable<CSTradeItem>> GetInventoryAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var url = $"{BaseUri}getInventory?order_by=price_desc&bot=all";
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var textJson = await response.Content.ReadAsStringAsync();
+                var responseJson = JsonSerializer.Deserialize<CSTradeInventoryDataResponse>(textJson);
+                return responseJson?.Inventory;
+            }
+        }
+    }
+}
