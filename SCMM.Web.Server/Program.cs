a48031f4bff0cdb8732e73061cb298690e1193d1
Rhys Koedijk <rhys@koedijk.co.nz>
2022-09-11 20:00:18 +1200

    Fix for 3D item viewer being blocked by content-security-policy

--- SCMM.Web.Server/Program.cs
+++ SCMM.Web.Server/Program.cs
@@ -339,7 +339,7 @@ public static class WebApplicationExtensions
             cspScriptSources: "'self' 'unsafe-inline' 'unsafe-eval' cdnjs.cloudflare.com cdn.jsdelivr.net cdn.skypack.dev www.googletagmanager.com www.google-analytics.com",
             cspStyleSources: "'self' 'unsafe-inline' cdnjs.cloudflare.com fonts.googleapis.com www.google-analytics.com",
             cspFontSources: "'self' data: cdnjs.cloudflare.com fonts.gstatic.com",
-            cspImageSources: "'self' *.scmm.app steamcommunity-a.akamaihd.net steamuserimages-a.akamaihd.net steamcdn-a.akamaihd.net avatars.steamstatic.com avatars.akamai.steamstatic.com cdn.discordapp.com www.google-analytics.com",
+            cspImageSources: "'self' data: blob: *.scmm.app steamcommunity-a.akamaihd.net steamuserimages-a.akamaihd.net steamcdn-a.akamaihd.net avatars.steamstatic.com avatars.akamai.steamstatic.com cdn.discordapp.com www.google-analytics.com",
             cspFrameSources: "'self' www.youtube.com",
             cspConnectSources: $"'self' *.scmm.app discordapp.com www.google-analytics.com stats.g.doubleclick.net {allowLoopbackConnectHack}",
             cspAllowCrossOriginEmbedding: true
