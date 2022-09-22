54385aeaeac1c467923f16166a655f2f0ad59b4f
Rhys Koedijk <rhys@koedijk.co.nz>
2022-09-22 21:28:04 +1200

    Job schedule tweaks

--- SCMM.Steam.Functions/Timer/CheckForNewMarketItems.cs
+++ SCMM.Steam.Functions/Timer/CheckForNewMarketItems.cs
@@ -37,7 +37,7 @@ public class CheckForNewMarketItems
     }
 
     [Function("Check-New-Market-Items")]
-    public async Task Run([TimerTrigger("0 3 * * * *")] /* every hour, 3 minutes after the hour */ TimerInfo timerInfo, FunctionContext context)
+    public async Task Run([TimerTrigger("0 0-15 12 * * *")] /* every minute, between 12:00 PM and 12:15 PM */ TimerInfo timerInfo, FunctionContext context)
     {
         var logger = context.GetLogger("Check-New-Market-Items");
 
