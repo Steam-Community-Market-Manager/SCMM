d2c3b9b71bac693ca48fffb10ee5e3a3885194a8
Rhys Koedijk <rhys@koedijk.co.nz>
2023-02-10 10:05:01 +1300

    Add enumerable extension to find median value

--- SCMM.Web.Data.Models/Extensions/EnumerableExtensions.cs
+++ SCMM.Web.Data.Models/Extensions/EnumerableExtensions.cs
@@ -14,5 +14,30 @@ namespace SCMM.Web.Data.Models.Extensions
             var filterWords = filterBy.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
             return source.Where(x => filterWords.All(word => x.Filters?.Any(f => f?.Contains(word, StringComparison.InvariantCultureIgnoreCase) == true) == true));
         }
+
+        public static TValue Median<TColl, TValue>(this IEnumerable<TColl> source, Func<TColl, TValue> selector)
+        {
+            return source.Select<TColl, TValue>(selector).Median();
+        }
+
+        public static T Median<T>(this IEnumerable<T> source)
+        {
+            var array = source.ToArray();
+            if (Nullable.GetUnderlyingType(typeof(T)) != null)
+            {
+                array = array.Where(x => x != null).ToArray();
+            }
+
+            var count = array.Count();
+            if (count == 0)
+            {
+                return default(T);
+            }
+
+            var midpoint = (int) Math.Round((decimal)count / 2, 0);
+            return array
+                .OrderBy(n => n)
+                .ElementAt(midpoint);
+        }
     }
 }
