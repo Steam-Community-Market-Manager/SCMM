8f0199fe41bc8a3550672453c3624101c0356744
Rhys Koedijk <rhys@koedijk.co.nz>
2023-05-17 09:37:57 +1200

    Brain fart

---
+++ SCMM.Steam.Data.Store/Migrations/20230516212149_SteamAssetDescriptionSupplyTotalDrops.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SCMM.Steam.Data.Store.Migrations
+{
+    /// <inheritdoc />
+    public partial class SteamAssetDescriptionSupplyTotalDrops : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "SupplyTotalDropsEstimated",
+                table: "SteamAssetDescriptions",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "SupplyTotalDropsKnown",
+                table: "SteamAssetDescriptions",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SupplyTotalDropsEstimated",
+                table: "SteamAssetDescriptions");
+
+            migrationBuilder.DropColumn(
+                name: "SupplyTotalDropsKnown",
+                table: "SteamAssetDescriptions");
+        }
+    }
+}
