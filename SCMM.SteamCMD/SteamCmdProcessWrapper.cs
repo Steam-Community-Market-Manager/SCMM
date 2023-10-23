08d8f71a7946eb3fa88e81a5cb157b09fda09c21
Rhys Koedijk <rhys@koedijk.co.nz>
2023-10-23 23:39:21 +1300

    Handle SteamCMD errors properly (#23)
    * Clear the SteamCMD cache before downloading workshop files
    
    * Handle known SteamCMD errors properly

--- SCMM.SteamCMD/SteamCmdProcessWrapper.cs
+++ SCMM.SteamCMD/SteamCmdProcessWrapper.cs
@@ -4,6 +4,7 @@ using SCMM.Steam.Abstractions;
 using System.Diagnostics;
 using System.IO.Compression;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace SCMM.SteamCMD;
 public class SteamCmdProcessWrapper : ISteamConsoleClient
@@ -15,7 +16,7 @@ public class SteamCmdProcessWrapper : ISteamConsoleClient
         _logger = logger;
     }
 
-    public async Task<WebFileData?> DownloadWorkshopFile(string appId, string workshopFileId)
+    public async Task<WebFileData?> DownloadWorkshopFile(string appId, string workshopFileId, bool clearCache = true)
     {
         var workshopFileName = $"{appId}-{workshopFileId}.zip";
         var workshopFileBasePath = $"Tools{Path.DirectorySeparatorChar}steamapps{Path.DirectorySeparatorChar}workshop{Path.DirectorySeparatorChar}content{Path.DirectorySeparatorChar}{appId}{Path.DirectorySeparatorChar}{workshopFileId}";
@@ -38,13 +39,34 @@ public class SteamCmdProcessWrapper : ISteamConsoleClient
                 throw new PlatformNotSupportedException($"Workshop file download failed, OS platform is not supported");
             }
 
+            if (clearCache)
+            {
+                // Sometimes Steam will fail with error "Download item {workshopFileId} failed (Failure)".
+                // This is normally a temporary error which can be fixed by deleting the cached download and trying again
+                var cachedPaths = new string[]
+                {
+                    $"Tools{Path.DirectorySeparatorChar}appcache",
+                    $"Tools{Path.DirectorySeparatorChar}steamapps",
+                    $"Tools{Path.DirectorySeparatorChar}userdata"
+                };
+                foreach (var cachedPath in cachedPaths)
+                {
+                    if (Path.Exists(cachedPath))
+                    {
+                        Directory.Delete(cachedPath, true);
+                    }
+                }
+            }
+
             var downloadProcess = Process.Start(new ProcessStartInfo()
             {
                 FileName = steamCmdPath,
                 Arguments = $"+login anonymous +workshop_download_item {appId} {workshopFileId} +quit",
                 UseShellExecute = false,
                 CreateNoWindow = true,
-                WindowStyle = ProcessWindowStyle.Hidden
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
             });
             if (downloadProcess == null)
             {
@@ -57,10 +79,22 @@ public class SteamCmdProcessWrapper : ISteamConsoleClient
                 throw new Exception($"Workshop file download failed, process exited with code {downloadProcess.ExitCode}");
             }
 
+            // If the workshop file did NOT get downloaded, find out why...
             if (!Directory.Exists(workshopFileBasePath))
             {
-                _logger.LogWarning("Workshop file cannot be found, it either has been deleted or is private");
-                return default;
+                var downloadProcessOutput = downloadProcess.StandardOutput.ReadToEnd();
+                var errorReason = Regex.Match(downloadProcessOutput, @"ERROR! (.*) \((.*)\)\.").Groups.OfType<Capture>().LastOrDefault()?.Value;
+                switch (errorReason)
+                {
+                    case "Access Denied":
+                        throw new UnauthorizedAccessException($"Workshop file download failed, file is private or hidden");
+                    case "File Not Found":
+                        throw new FileNotFoundException($"Workshop file download failed, file has been deleted");
+                    case "Failure":
+                        throw new IOException($"Workshop file download failed, generic download failure, try again later");
+                    default:
+                        throw new Exception($"Workshop file download failed, process completed successfully but no file was downloaded. Error reason was: '{errorReason}'");
+                }
             }
 
             ZipFile.CreateFromDirectory(workshopFileBasePath, workshopFileZipPath, CompressionLevel.SmallestSize, includeBaseDirectory: false);
