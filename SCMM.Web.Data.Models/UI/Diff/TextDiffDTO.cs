d0883c80157af0fb40bec0676d7782fa1fced3fa
Rhys Koedijk <rhys@koedijk.co.nz>
2024-01-15 18:33:14 +1300

    Item definition archives text diff viewer (#69)
    * NuGet package updates
    
    * Item definition archive are now displayed as a text diff from the previous archive
    
    * Tidy up item definition archive page, add download button
    
    * Download item definition archives in their raw JSON format
    
    * Remove unused variables
    
    * Show archive date time

---
+++ SCMM.Web.Data.Models/UI/Diff/TextDiffDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SCMM.Web.Data.Models.UI.Diff;
+
+public class TextDiffDTO
+{
+    public TextDiffPieceDTO[] Lines { get; set; }
+
+    public bool HasDifferences => Lines.Any(x => x.Type != TextDiffChangeType.Unchanged.ToString());
+}
---
+++ SCMM.Web.Data.Models/UI/Diff/TextDiffPieceDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace SCMM.Web.Data.Models.UI.Diff;
+
+public class TextDiffPieceDTO
+{
+    public string Type { get; set; }
+
+    public int? Position { get; set; }
+
+    public string Text { get; set; }
+
+    public TextDiffPieceDTO[] SubPieces { get; set; }
+
+}
