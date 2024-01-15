b2f4ad3bb3a591d9b58a19f8ec37be94d2dd04dd
Rhys Koedijk <rhys@koedijk.co.nz>
2024-01-15 23:40:22 +1300

    Add RapidSkins market web client

---
+++ SCMM.Market.RapidSkins.Client/RapidSkinsItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SCMM.Market.RapidSkins.Client
+{
+    public class RapidSkinsItem
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("amount")]
+        public long Amount { get; set; }
+
+        [JsonPropertyName("price")]
+        public long Price { get; set; }
+    }
+}
---
+++ SCMM.Market.RapidSkins.Client/RapidSkinsWebClient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+
+namespace SCMM.Market.RapidSkins.Client
+{
+    public class RapidSkinsWebClient : Shared.Web.Client.WebClientBase
+    {
+        private const string WebBaseUri = "https://rapidskins.com/";
+
+        public RapidSkinsWebClient(ILogger<RapidSkinsWebClient> logger) : base(logger) { }
+
+        public async Task<RapidSkinsInventoryResponse> GetInventoryAsync()
+        {
+            using (var client = BuildWebBrowserHttpClient(referrer: new Uri(WebBaseUri)))
+            {
+                var url = $"{WebBaseUri}api/v1/site/inventory";
+                var response = await RetryPolicy.ExecuteAsync(() => client.GetAsync(url));
+                response.EnsureSuccessStatusCode();
+
+                var textJson = await response.Content.ReadAsStringAsync();
+                if (string.IsNullOrEmpty(textJson))
+                {
+                    return default;
+                }
+
+                var responseJson = JsonSerializer.Deserialize<RapidSkinsInventoryResponse>(textJson);
+                return responseJson;
+            }
+        }
+    }
+}
