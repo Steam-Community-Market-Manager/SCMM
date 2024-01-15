9544d9909e51fbd4482caad8184684687dd6583a
Rhys Koedijk <rhys@koedijk.co.nz>
2024-01-15 22:45:03 +1300

    Add Waxpeer market web client

---
+++ SCMM.Market.Waxpeer.Client/WaxpeerWebClient.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace SCMM.Market.Waxpeer.Client
+{
+    public class WaxpeerWebClient : Shared.Web.Client.WebClientBase
+    {
+        private const string ApiBaseUri = "https://api.waxpeer.com/";
+
+        public WaxpeerWebClient(ILogger<WaxpeerWebClient> logger) : base(logger) 
+        {
+        }
+
+        /// <see cref="https://docs.waxpeer.com/?method=v1-prices-get"/>
+        public async Task<WaxpeerPricesResponse> GetPricesAsync(string appName)
+        {
+            using (var client = BuildWebApiHttpClient())
+            {
+                var url = $"{ApiBaseUri}v1/prices/items?game={appName.ToLower()}t&minified=1";
+                var response = await RetryPolicy.ExecuteAsync(() => client.GetAsync(url));
+                response.EnsureSuccessStatusCode();
+
+                var textJson = await response.Content.ReadAsStringAsync();
+                if (string.IsNullOrEmpty(textJson))
+                {
+                    return default;
+                }
+
+                var responseJson = JsonSerializer.Deserialize<WaxpeerPricesResponse>(textJson);
+                return responseJson;
+            }
+        }
+    }
+}
