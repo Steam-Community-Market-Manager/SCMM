712c3e40fc4edb44dcbb26d7a047bac7c8479756
Rhys Koedijk <rhys@koedijk.co.nz>
2024-01-30 00:18:25 +1300

    Remove SteamProfileConfiguration from SteamDB model

---
+++ SCMM.Steam.Data.Store/Migrations/20240129111643_RemoveSteamProfileConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SCMM.Steam.Data.Store.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveSteamProfileConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SteamProfileConfiguration");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SteamProfileConfiguration",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    List_Serialised = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SteamProfileConfiguration", x => x.Id);
+                });
+        }
+    }
+}
