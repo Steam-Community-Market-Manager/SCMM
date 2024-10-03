9e49678619f2500a1d0fe4e41a2ba0d11efe1c2f
Rhys Koedijk <rhys@koedijk.co.nz>
2024-10-03 23:58:31 +1300

    Add subscriber graph to store analytics panel

---
+++ SCMM.Web.Data.Models/UI/Statistic/StoreItemSubscriberSnapshotDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SCMM.Web.Data.Models.UI.Statistic
+{
+    public class StoreItemSubscriberSnapshotDTO
+    {
+        public DateTime Timestamp { get; set; }
+
+        public ulong Subscribers { get; set; }
+    }
+}
---
+++ SCMM.Web.Data.Models/UI/Statistic/StoreItemSubscriptionDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace SCMM.Web.Data.Models.UI.Statistic
+{
+    public class StoreItemSubscriptionDTO
+    {
+        public string Name { get; set; }
+
+        public string IconAccentColour { get; set; }
+
+        public string IconUrl { get; set; }
+
+        public int Position { get; set; }
+
+        public List<StoreItemSubscriberSnapshotDTO> SubscriberTimeline { get; set; }
+    }
+}
